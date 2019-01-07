5961650973a4321061780d3b7bf2330b9dceeae7
mitsui-wang <340583503@qq.com>
2019-01-08 00:21:23 +0800

    provider

--- eureka-server/src/main/java/com/mitsui/spring/eurekaserver/controller/MainController.java
+++ eureka-server/src/main/java/com/mitsui/spring/eurekaserver/controller/MainController.java
@@ -1,9 +1,8 @@
-package com.mitsui.spring.controller;
+package com.mitsui.spring.eurekaserver.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 
 
 @Controller
