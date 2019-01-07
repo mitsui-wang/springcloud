d81b831a0e8a4b2bfc2360f159c915612a31001b
mitsui-wang <340583503@qq.com>
2019-01-07 22:04:18 +0800

    first

---
+++ src/main/java/com/dtdream/login/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.dtdream.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+@Controller
+@RequestMapping("/main")
+public class MainController {
+
+	@RequestMapping("/index")
+	@ResponseBody
+	public String index() {
+		return "index12";
+	}
+
+}
